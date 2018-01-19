2507587b2b7c3bb8affd1ee443330b1384973001
Robby <ta7382@gmail.com>
2018-01-20 02:18:11 +0800

    use fetch to add data

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,25 @@
+function Add() {
+
+    const item = document.querySelector('#itemName').value;
+
+    if (item == '') {
+        alert('Please input the item name.');
+    } else {
+        fetch(`/api/todo/`, {
+            method: 'POST',
+            body: JSON.stringify({
+                item: item
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            window.location.reload();
+        });
+    }
+}
+
 function Delete(id) {
     fetch(`/api/todo/${id}`, {
         method: 'DELETE',
@@ -10,7 +32,10 @@ function Checked(id, done) {
     console.log(id, !done);
     fetch(`/api/todo/`, {
         method: 'PUT',
-        body: JSON.stringify({ id: id, done: !done }),
+        body: JSON.stringify({
+            id: id,
+            done: !done
+        }),
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,24 @@ const asyncfunc = (fn) => {
 }
 
 /* GET home page. */
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
 
   let doc = await Todo.find();
-  res.render('index', { route: 'home', data: doc });
+  res.render('index', {
+    route: 'home',
+    data: doc
+  });
 });
 
-router.get('/api/todo', async (req, res) => {
-  res.json({ status: "ok" });
+router.get('/api/todo', async(req, res) => {
+  res.json({
+    status: "ok"
+  });
 });
 
-router.post('/api/todo', async (req, res) => {
+router.post('/api/todo', async(req, res) => {
+
+  console.log(req.body)
   const myTodo = new Todo({
     item: req.body.item,
     done: false,
@@ -31,24 +38,21 @@ router.post('/api/todo', async (req, res) => {
   //   res.json(doc.toJSON());
   // }); 
   const doc = await myTodo.save();
-  // res.json(doc.toJSON());
-  res.redirect('/');
-});
-
-router.put('/api/todo', async function (req, res, next) {
-  console.log(req.body);
-  const doc = await Todo.findByIdAndUpdate(req.body.id, { done: req.body.done });
   console.log(doc);
   res.json(doc);
 });
 
-router.patch('/api/todo', function (req, res, next) {
-  console.log('patch');
-  res.json(req.body);
+router.put('/api/todo', async function (req, res, next) {
+  const doc = await Todo.findByIdAndUpdate(req.body.id, {
+    done: req.body.done
+  });
+  console.log(doc);
+  res.json(doc);
 });
 
 router.delete('/api/todo/:id', async function (req, res, next) {
   const doc = await Todo.findByIdAndRemove(req.params.id);
+  console.log(doc);
   res.json(doc);
 });
 
